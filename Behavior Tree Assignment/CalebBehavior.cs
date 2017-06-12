cf9219ba5c658704961f28629d41b273ec5e9599
Caleb J. Yu <calebyu33@gmail.com>
2017-06-12 12:07:03 -0400

    behavior trees

---
+++ Behavior Tree Assignment/CalebBehavior.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using NPC;
+using TreeSharpPlus;
+
+public class CalebBehavior : MonoBehaviour {
+
+    //public NPCController agent;
+    public NPCBehavior AgentA;
+    public NPCBehavior AgentB;
+    public NPCBehavior AgentC;
+    public Transform target;
+    public Transform target2;
+    public Transform target3;
+    public Transform gorgonfront;
+    public Transform gorgon;
+    public GameObject bubbleA;
+    public GameObject bubbleB;
+    public GameObject bubbleC;
+
+    private BehaviorAgent behaviorAgent;
+    private IEnumerable<IHasBehaviorObject> NPCarray;
+    private bool AgentAUpset = false;
+    private bool AgentBUpset = false;
+    private bool AgentCUpset = false;
+
+    // Use this for initialization
+    void Start () {
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (bubbleA.activeSelf)
+        {
+            if (Input.GetKey(KeyCode.Return))
+            {
+                AgentA.hasRead = true;
+                Debug.Log(AgentA.hasRead);
+            }
+        }
+        if (bubbleB.activeSelf)
+        {
+            if (Input.GetKey(KeyCode.Return))
+            {
+                AgentB.hasRead = true;
+            }
+        }
+        if (bubbleC.activeSelf)
+        {
+            if (Input.GetKey(KeyCode.Return))
+            {
+                AgentC.hasRead = true;
+            }
+        }
+    }
+    protected Node ApproachAndWait(Transform target)
+    {
+        return new Sequence(AgentA.NPCBehavior_GoTo(target, true), new LeafWait(1000));
+    }
+    protected Node ChoosePartner()
+    {
+        if (UnityEngine.Random.Range(-1f, 1f) > 0)
+        {
+            AgentCUpset = true;
+            return new Sequence(
+                AgentA.NPCBehavior_OrientTowards(AgentB.transform),
+                AgentA.NPCBehavior_Talk("Hi Danny Bones! I like you better than Steve!", bubbleA),
+                AgentB.NPCBehavior_Talk("Thanks Manny Bones!", bubbleB),
+                AgentC.NPCBehavior_DoGesture(GESTURE_CODE.DISSAPOINTMENT)
+            );
+        }else {
+            AgentBUpset = true;
+            return new Sequence(
+                AgentA.NPCBehavior_OrientTowards(AgentC.transform),
+                AgentA.NPCBehavior_Talk("Hi Steve! I like you better than Danny Bones!", bubbleA),
+                AgentC.NPCBehavior_Talk("Thanks Manny Bones!",bubbleC),
+                AgentB.NPCBehavior_DoGesture(GESTURE_CODE.DISSAPOINTMENT)
+            );
+        }
+                            
+    }
+    protected Node WalkAway()
+    {
+        if (AgentBUpset)
+        {
+            return new Sequence(
+                new SequenceParallel(
+                    AgentA.NPCBehavior_GoTo(target, false),
+                    AgentC.NPCBehavior_GoTo(target, false)
+                ),
+                new SequenceParallel(
+                    AgentA.NPCBehavior_OrientTowards(AgentC.transform),
+                    AgentC.NPCBehavior_OrientTowards(AgentA.transform)
+                ),
+                AgentB.NPCBehavior_GoTo(gorgonfront, true),
+                AgentB.NPCBehavior_OrientTowards(gorgon),
+                AgentB.NPCBehavior_Talk("I am going to kill them!", bubbleB)
+            );
+        }else if (AgentCUpset)
+        {
+            return new Sequence(
+                new SequenceParallel(
+                    AgentA.NPCBehavior_GoTo(target3, false),
+                    AgentB.NPCBehavior_GoTo(target3, false)
+                ),
+                new SequenceParallel(
+                    AgentA.NPCBehavior_OrientTowards(AgentB.transform),
+                    AgentB.NPCBehavior_OrientTowards(AgentA.transform)
+                ),
+                AgentC.NPCBehavior_GoTo(gorgonfront, true),
+                AgentC.NPCBehavior_OrientTowards(gorgon),
+                AgentC.NPCBehavior_Talk("I am going to kill them!", bubbleC)
+            );
+        }
+        return null;
+    }
+    protected Node Revenge()
+    {
+        if (AgentBUpset)
+        {
+            return new Sequence(
+                AgentB.NPCBehavior_GoTo(AgentA.transform, true),
+                AgentB.NPCBehavior_DoGesture(GESTURE_CODE.GRAB_FRONT),
+                die("a"),
+                AgentB.NPCBehavior_GoTo(AgentC.transform, true),
+                AgentB.NPCBehavior_DoGesture(GESTURE_CODE.GRAB_FRONT),
+                die("c")
+            );
+        }
+        else if (AgentCUpset)
+        {
+            return new Sequence(
+                AgentC.NPCBehavior_GoTo(AgentA.transform, true),
+                AgentC.NPCBehavior_DoGesture(GESTURE_CODE.GRAB_FRONT),
+                die("a"),
+                AgentC.NPCBehavior_GoTo(AgentB.transform, true),
+                AgentC.NPCBehavior_DoGesture(GESTURE_CODE.GRAB_FRONT),
+                die("b")
+            );
+        }
+        return null;
+    }
+    protected Node Pursuit()
+    {
+        if (AgentBUpset)
+        {
+            return new DecoratorLoop(
+                updatePursuit("c")
+            );
+        }
+        else if (AgentCUpset)
+        {
+            return new DecoratorLoop(
+                updatePursuit("b")
+            );
+        }
+        return null;
+    }
+    private Node updatePursuit(String agent)
+    {
+        target2.position.Set(UnityEngine.Random.Range(-50, 50), 0, UnityEngine.Random.Range(-50, 50));
+        if (agent.Equals("c"))
+            return new SequenceParallel(AgentC.NPCBehavior_GoTo(target2, true), new Sequence(new LeafWait(10000),AgentB.NPCBehavior_GoTo(AgentC.transform, true),die("c"),AgentB.NPCBehavior_DoGesture(GESTURE_CODE.HURRAY)));
+        else
+            return new SequenceParallel(AgentB.NPCBehavior_GoTo(target2, true), new Sequence(new LeafWait(10000), AgentC.NPCBehavior_GoTo(AgentB.transform, true),die("b"),AgentC.NPCBehavior_DoGesture(GESTURE_CODE.HURRAY)));
+    }
+    private Node die(string agent)
+    {
+        if (agent.Equals("c"))
+        {
+            //AgentC.StopAllCoroutines(); is there a way to stop all behaviors for this agent?
+            return AgentC.NPCBehavior_DoGesture(GESTURE_CODE.DIE);
+        }
+        else if(agent.Equals("b"))
+        {
+            //AgentB.StopAllCoroutines();
+            return AgentB.NPCBehavior_DoGesture(GESTURE_CODE.DIE);
+        }else
+        {
+            //AgentA.StopAllCoroutines();
+            return AgentA.NPCBehavior_DoGesture(GESTURE_CODE.DIE);
+        }
+    }
+    protected Node BuildTreeRoot()
+    {
+        Node root = new Sequence(
+                        AgentB.NPCBehavior_OrientTowards(AgentA.transform),
+                        AgentC.NPCBehavior_OrientTowards(AgentA.transform),
+                        ChoosePartner(),
+                        WalkAway(),
+                        new LeafWait(2000),
+                        Revenge()
+                        //Pursuit()
+                    );
+        return root;
+    }
+}
---
+++ Behavior Tree Assignment/NPCBehavior.cs
@@ -0,0 +1,286 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TreeSharpPlus;
+
+using NPC;
+using System;
+
+/// <summary>
+/// We will use this module as the connector of NPC with
+/// TreeSharpPlus. The main purpose of this module is to
+/// be a shell for the already implemented affordances of
+/// the agents. Nodes will tick here and the module on each
+/// tick will return the current status of each node.
+/// 
+/// Note that for each affordance, we will need two functions:
+///     1. Node return to create the actual Behavior Tree component
+///     2. A wrapper controller on the affordance which will be ticked on
+///        and need to return, on tick, the status of the node.
+/// Although this will add an extra layer of complexity, I will concentrate
+/// all the implementation on this module rather than mixing TreeSharpPlus
+/// with the NPC affordances. That will allow, in the long run, for more
+/// freedom, scalable, maitainable and clear code.
+/// 
+/// </summary>
+
+public class NPCBehavior : MonoBehaviour, INPCModule, IHasBehaviorObject {
+
+    #region Members
+    
+    private NPCController g_NPCController;
+    public bool Enabled = true;
+    public bool hasRead = false;
+    private bool g_GestureRunning = false;
+    BehaviorObject g_BehaviorObject;
+    private bool g_Initialized = false;
+    private Transform[] g_RoamingPoints;
+
+    public BehaviorObject Behavior {
+        get {
+            return g_BehaviorObject;
+        }
+    }
+
+    #endregion
+
+    #region Unity_Methods
+
+    public void Awake() { }
+
+    #endregion
+
+    #region Public_Functions
+    /// <summary>
+    /// Void methods are self contained behaviors of an agent
+    /// </summary>
+    /// <param name="code"></param>
+    public void Behavior_DoTimedGesture(GESTURE_CODE code) {
+        BehaviorEvent be = new BehaviorEvent(doEvent => NPCBehavior_DoTimedGesture(code), new IHasBehaviorObject[] { this });
+        be.StartEvent(1f);
+    }
+
+    public Node ApproachAndWait(Transform target, bool run) {
+        return new Sequence(NPCBehavior_GoTo(target, run), new LeafWait(1000));
+    }
+
+    public Node NPCBehavior_TakeSit(NPCObject chair) {
+        Transform t = chair.MainInteractionPoint;
+        return NPCBehavior_TakeSit(t);
+    }
+
+    public Node NPCBehavior_TakeSit(Transform t) {
+        return new Sequence(
+                NPCBehavior_GoTo(t, false),
+                NPCBehavior_OrientTowards(t.position + t.forward),
+                NPCBehavior_DoTimedGesture(GESTURE_CODE.SIT),
+                NPCBehavior_DoGesture(GESTURE_CODE.SITTING,true)
+            );
+    }
+
+    public Node NPCBehavior_PatrolRandomPoints(Transform[] points) {
+        g_RoamingPoints = g_RoamingPoints == null ? points : g_RoamingPoints;
+        return new DecoratorLoop (
+            new SequenceShuffle(
+                new LeafInvoke(() => Behavior_DoGesture(GESTURE_CODE.LOOK_AROUND)),
+                new LeafWait(3500),
+                new LeafInvoke(() => Behavior_Wander(false)),
+                new LeafWait(2500),
+                new LeafInvoke(() => Behavior_DoGesture(GESTURE_CODE.THINK))
+            )
+        );
+    }
+
+    public Node NPCBehavior_OrientTowards(Transform t) {
+        Debug.Log("OrientTowards");
+        return new LeafInvoke(() => Behavior_OrientTowards(t));
+    }
+
+    public Node NPCBehavior_OrientTowards(Vector3 t) {
+        return new LeafInvoke(() => Behavior_OrientTowards(t));
+    }
+
+    public Node NPCBehavior_LookAt(Transform t, bool start) {
+        if(start)
+            return new LeafInvoke(() => Behavior_LookAt(t));
+        else
+            return new LeafInvoke(() => Behavior_StopLookAt());
+    }
+
+    public Node NPCBehavior_GoTo(Transform t, bool run) {
+        g_NPCController.Debug(g_NPCController + " GoTo: " + t);
+        return new LeafInvoke(
+            () => Behavior_GoTo(t, run)                           
+        );
+    }
+
+    public Node NPCBehavior_DoTimedGesture(GESTURE_CODE gesture, System.Object o = null) {
+        return NPCBehavior_DoGesture(gesture, o, true);
+    }
+
+    public Node NPCBehavior_DoGesture(GESTURE_CODE gesture, System.Object o = null, bool timed = false) {
+        return new LeafInvoke(
+            () => Behavior_DoGesture(gesture,o, timed)
+        );
+    }
+    //---------------------------Caleb's affordances
+    public Node NPCBehavior_Talk(String speech, GameObject bubble)
+    {
+        return new Sequence(
+                new LeafInvoke(() => Behavior_Talk(speech, bubble))
+                //new LeafWait(2000)
+            ); 
+    }
+    //---------------------------
+    public override string ToString() {
+        return g_NPCController.name;
+    }
+
+    #endregion
+
+    #region Private_Functions
+    //---------------------------Caleb's affordances
+    private RunStatus Behavior_Talk(String speech, GameObject bubble)
+    {
+            bubble.SetActive(true);
+            bubble.GetComponentInChildren<Text>().text = speech;
+            if (hasRead)
+            {
+                Debug.Log("hasRead");
+                bubble.SetActive(false);
+                hasRead = false;
+                return RunStatus.Success;
+            }else
+            {
+                Debug.Log("reading");
+                return RunStatus.Running;
+            }
+    }
+    //---------------------------
+    private RunStatus Behavior_OrientTowards(Vector3 t) {
+        if (g_NPCController.Body.TargetOrientation != t) {
+            g_NPCController.OrientTowards(t);
+        }
+        return g_NPCController.Body.Oriented ? RunStatus.Success : RunStatus.Running;
+    }
+
+    private RunStatus Behavior_OrientTowards(Transform t) {
+        if(g_NPCController.Body.TargetOrientation != t.position ) {
+            g_NPCController.OrientTowards(t.position);
+        }
+        return g_NPCController.Body.Oriented ? RunStatus.Success : RunStatus.Running;
+    }
+
+    private RunStatus Behavior_DoGesture(GESTURE_CODE gest, System.Object o = null, bool timed = false) {
+        if (g_NPCController.Body.IsGesturePlaying(gest)) {
+            return RunStatus.Running;
+        } else if (g_GestureRunning) {
+            g_GestureRunning = false;
+            return RunStatus.Success;
+        }  else {
+            try {
+                g_NPCController.Body.DoGesture(gest, o, timed);
+                g_GestureRunning = true;
+                return RunStatus.Running;
+            } catch (System.Exception e ) {
+                g_NPCController.Debug("Could not initialize gesture with error: " + e.Message);
+                return RunStatus.Failure;
+            }
+        }
+        
+    }
+
+    private RunStatus Behavior_Wander(bool run) {
+        if (!g_NPCController.Body.Navigating) {
+            try {
+                Transform t = g_RoamingPoints[(int)(UnityEngine.Random.value * (g_RoamingPoints.Length - 1))];
+                if (run)
+                    g_NPCController.RunTo(t.position);
+                else g_NPCController.GoTo(t.position);
+                return RunStatus.Running;
+            } catch (System.Exception e) {
+                g_NPCController.Debug(e.Message);
+                return RunStatus.Failure;
+            }
+        } else { return RunStatus.Success; }
+    }
+
+    private RunStatus Behavior_GoTo(Transform t, bool run) {
+        if (g_NPCController.Body.IsAtTargetLocation(t.position)) {
+            g_NPCController.Debug("Finished go to");
+            return RunStatus.Success;
+        }
+        else {
+            try {
+                Debug.Log("going");
+                if (run)
+                    g_NPCController.RunTo(t.position);
+                else g_NPCController.GoTo(t.position);
+                return RunStatus.Running;
+            } catch(System.Exception e) {
+                // this will occur if the target is unreacheable
+                Debug.Log("failure");
+                return RunStatus.Failure;
+            }
+        }
+    }
+
+    private RunStatus Behavior_StopLookAt() {
+        g_NPCController.Body.StopLookAt();
+        return RunStatus.Success;
+    }
+
+    private RunStatus Behavior_LookAt(Transform t) {
+        g_NPCController.Body.StartLookAt(t);
+        g_NPCController.Debug("Finished look at");
+        return RunStatus.Success;
+    }
+
+    #endregion
+
+    #region INPCModule
+
+    public void InitializeModule() {
+        g_NPCController = GetComponent<NPCController>();
+        g_BehaviorObject = new BehaviorAgent(
+                                new DecoratorLoop(
+                                    new LeafAssert(() => true)));
+        BehaviorManager.Instance.Register((IBehaviorUpdate)g_BehaviorObject);
+        g_NPCController.Debug("NPCBehavior - Initialized: " + name);
+        g_Initialized = true;
+    }
+
+    public bool IsEnabled() {
+        return Enabled;
+    }
+
+    public string NPCModuleName() {
+        return "NPC TreeSP/Connector";
+    }
+
+    public NPC_MODULE_TARGET NPCModuleTarget() {
+        return NPC_MODULE_TARGET.AI;
+    }
+
+    public NPC_MODULE_TYPE NPCModuleType() {
+        return NPC_MODULE_TYPE.BEHAVIOR;
+    }
+
+    public void RemoveNPCModule() {
+        throw new NotImplementedException();
+    }
+
+    public void SetEnable(bool e) {
+        Enabled = e;
+    }
+
+    public bool IsUpdateable() {
+        return false;
+    }
+
+    public void TickModule() {
+        throw new NotImplementedException();
+    }
+    
+    #endregion
+}
